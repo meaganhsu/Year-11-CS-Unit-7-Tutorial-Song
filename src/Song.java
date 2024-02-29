26cbfbfb6c22b754f0ea933f28fd1d4f252dffb7
meaganhsu <myth72@student.cis.edu.hk>
2024-02-29 12:00:25 +0800

    done

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,29 @@
+import java.util.*;
 public class Song {
+    private ArrayList<String> listeners;
+    private String title;
+    private String artist;
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+        listeners = new ArrayList<>();
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int howMany(ArrayList<String> list) {
+        int count = 0;
 
-    private String
+        for (String s : list) {
+            String x = s.toLowerCase();
+            if (!listeners.contains(x)) {
+                listeners.add(x);
+                count++;
+            }
+        }
+        return count;
+    }
 }
